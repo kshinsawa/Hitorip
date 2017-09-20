667ebf50df502597c5d9ef50e56ff67bee1b4ef0
kshinsawa <kshinsawa@netprotections.co.jp>
2017-09-20 15:00:33 +0900

    特に変更なし

--- team_A/app/models/post.rb
+++ team_A/app/models/post.rb
@@ -17,11 +17,11 @@ class Post < ActiveRecord::Base
   has_many :evaluations
   belongs_to :user
 
-  validates_presence_of :title
-  validates_presence_of :image_name
-  validates_presence_of :content
-  validates_presence_of :date
-  validates_presence_of :place
-  validates_presence_of :category
+  # validates_presence_of :title
+  # validates_presence_of :image_name
+  # validates_presence_of :content
+  # validates_presence_of :date
+  # validates_presence_of :place
+  # validates_presence_of :category
 
 end
