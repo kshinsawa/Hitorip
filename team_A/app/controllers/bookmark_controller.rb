0bd960ffbadd00712e4b9f9ea37ef9fb62ed10bf
snozaki <snozaki@netprotections.co.jp>
2017-09-22 12:52:21 +0900

    途中保存

--- team_A/app/controllers/bookmark_controller.rb
+++ team_A/app/controllers/bookmark_controller.rb
@@ -4,7 +4,7 @@ class BookmarkController < ApplicationController
     @bookmark = Bookmark.find_by(user_id: @current_user.id)
     @post = Post.joins("INNER JOIN bookmarks ON bookmarks.post_id = posts.id").where("bookmarks.post_id = ?", @bookmark.post_id)
     @bookmark.destroy
-    redirect_to("/posts/#{@post.id}")
+    redirect_to("/user/#{@current_user.id}")
   end
 
 end
